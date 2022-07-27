ab08168384838ad70ecc275ca1e0a2ce40afa2f2
Sam Spencer <sam95@vt.edu>
2022-07-27 20:41:43 +0100

    Fixed the regex expression

--- AsciiArt.java
+++ AsciiArt.java
@@ -12,16 +12,14 @@ public class AsciiArt {
         //String newName = fileName.concat("_AsciiArt");
         // If there is a . in the file name, split string before renaming
         if (fileName.contains(".")){
-            String[] parts = fileName.split(".");
+            String[] parts = fileName.split("\\.");
             String prefix = parts[0];
             String fileExt = parts[1];
             String newName = prefix + asciiString + fileExt; 
-            System.out.print(newName);
             return newName;
         }
         else {
             String newName = fileName + asciiString; 
-            System.out.println(newName);
             return newName;
         }
     }
@@ -41,25 +39,25 @@ public class AsciiArt {
         
         
     }
-    public static void writeAnscii(BufferedImage targetImage, String fileName){
-        /* Need to get the height and width of the target image, then parse through */
-        int height = targetImage.getHeight();
-        int width = targetImage.getWidth();
-        String outputName  = outputFile(fileName);
-        File outputText = new File(outputName);
-        //HashMap asciiMap = new HashMap();
-        for (int h = 0; h < height; h++){
-            for (int w = 0; w< width; w++){
-                int rgb = targetImage.getRGB(h, w);
-                System.out.println(rgb);
-            }
-        }
+    // public static void writeAnscii(BufferedImage targetImage, String fileName){
+    //     /* Need to get the height and width of the target image, then parse through */
+    //     int height = targetImage.getHeight();
+    //     int width = targetImage.getWidth();
+    //     String outputName  = outputFile(fileName);
+    //     File outputText = new File(outputName);
+    //     //HashMap asciiMap = new HashMap();
+    //     for (int h = 0; h < height; h++){
+    //         for (int w = 0; w< width; w++){
+    //             int rgb = targetImage.getRGB(h, w);
+    //             System.out.println(rgb);
+    //         }
+    //     }
         
         // Define pixel to Ascii mapping here
         // Loop through height
         // Loop through Width
         //
-    }
+    //}
     public static void main(String[] args) {
         /*Create an output text file */
         try{
@@ -67,11 +65,12 @@ public class AsciiArt {
             // String fileName = args[0];
             BufferedImage targetFile = openFile(fileName);
             System.out.println("Corgi file opened");
+            System.out.print(fileName);
             String newName = outputFile(fileName);
             System.out.println("File Named");
             System.out.print(newName);
-            writeAnscii(targetFile, fileName);
-            System.out.println("Function called");
+            // writeAnscii(targetFile, fileName);
+            // System.out.println("Function called");
         }catch(final ArrayIndexOutOfBoundsException e){
             //System.out.println("No file selected");
             return;
