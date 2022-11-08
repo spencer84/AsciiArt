8df131bf3cf8dac2f6fcefa589971ea55cc5b2d6
Sam Spencer <sam95@vt.edu>
2022-11-08 22:00:39 +0000

    Added section to identify pct resize

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
 
     // Identify if file should be resized
-    public static boolean handleResize(){
+    public static boolean needsResize(){
         System.out.println("Re-size file? y/n");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {String input = reader.readLine();
@@ -15,19 +15,43 @@ public class Main {
             }
             else{
                 System.out.println("Input not recognised. Please input y or n...");
-                handleResize();
+                needsResize();
             }
         }
         catch (Exception e){
-            handleResize();
+            needsResize();
         }
             return false;
         }
 
+    public static int newSize(){
+        System.out.println("What percent (%) of the previous file size?");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {int input = Integer.valueOf(reader.readLine());
+
+            if(input > 0 && input <= 1000){
+                return input;
+            }
+            else{
+                System.out.println("Input must be a value between 1-1000");
+                newSize();
+            }
+        }
+        catch (Exception e){
+            newSize();
+        }
+            return 100;
+        }
 
         
     }
+
+        
+    
     public static void main(String[] args){
+        if(needsResize()){
+            Resizer resizer = new Resizer();
+        }
     // Do we need to re-size?
     // If no, then use the AsciiArt converter as defined
     // Otherwise 
