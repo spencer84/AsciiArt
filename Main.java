c605f13757293f038ffc91572d51ad734a3ef7fb
Sam Spencer <sam95@vt.edu>
2022-11-08 21:40:53 +0000

    Written section to ask user if file to be resized

--- Main.java
+++ Main.java
@@ -1,7 +1,38 @@
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args){
+    // Identify if file should be resized
+    public static boolean handleResize(){
+        System.out.println("Re-size file? y/n");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {String input = reader.readLine();
+            if( input.equals("y")){
+                return true;
+            }
+            else if  (input.equals("n")){
+                return false;
+            }
+            else{
+                System.out.println("Input not recognised. Please input y or n...");
+                handleResize();
+            }
+        }
+        catch (Exception e){
+            handleResize();
+        }
+            return false;
+        }
+
+
         
+    }
+    public static void main(String[] args){
+    // Do we need to re-size?
+    // If no, then use the AsciiArt converter as defined
+    // Otherwise 
+    AsciiArt art = new AsciiArt();
+    
     }
     
 }
