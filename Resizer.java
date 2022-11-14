970a14fe3c93abeb1e3b7be9dc5aa8e029d0037c
Sam Spencer <sam95@vt.edu>
2022-11-14 22:21:36 +0000

    Starting to build out the resizing logic

--- Resizer.java
+++ Resizer.java
@@ -3,13 +3,23 @@ import java.awt.image.BufferedImage;
 public class Resizer {
     private int height;
     private int width;
+    private int percent;
 
     public void setDimensions(int height, int width){
         this.height = height;
         this.width = width;
     }
+
+    public void setPercent(int pct){
+        this.percent = pct;
+    }
     
     public BufferedImage resize(BufferedImage image){
+        /* Need to get the height and width of the target image, then scale to output */
+        int height = image.getHeight();
+        int width = image.getWidth();
+        int outputHeight = height*(this.percent/100);
+        int outputWidth = width*(this.percent/100);
         // Add main logic to re-size the image here
         return image;
     }
