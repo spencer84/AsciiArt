1affd5a5eeacb78a2c81ef612bcb823577a873ea
Sam Spencer <sam95@vt.edu>
2022-11-15 20:52:40 +0000

    Opting to use the BufferedImage method to resize

--- Resizer.java
+++ Resizer.java
@@ -1,13 +1,14 @@
 import java.awt.image.BufferedImage;
+import java.awt.*;
 
 public class Resizer {
     private int height;
     private int width;
     private int percent;
 
-    public void setDimensions(int height, int width){
-        this.height = height;
-        this.width = width;
+    public void setDimensions(BufferedImage image){
+        this.height = image.getHeight();
+        this.width = image.getWidth();
     }
 
     public void setPercent(int pct){
@@ -16,11 +17,20 @@ public class Resizer {
     
     public BufferedImage resize(BufferedImage image){
         /* Need to get the height and width of the target image, then scale to output */
-        int height = image.getHeight();
-        int width = image.getWidth();
-        int outputHeight = height*(this.percent/100);
-        int outputWidth = width*(this.percent/100);
-        // Add main logic to re-size the image here
+        setDimensions(image);
+        int outputHeight = this.height*(this.percent/100);
+        int outputWidth = this.width*(this.percent/100);
+        // Try the BufferedImage method to resize
+        Image resized = image.getScaledInstance(outputWidth, outputHeight, image.SCALE_SMOOTH);
+
+        // Iterate through each pixel, convert to average pixel color
+        for (int w = 0; w< width; w++){
+            // For every line of pixels, initialize a new line of text to be written into the file
+            String line = System.lineSeparator() + "";
+            for (int h = 0; h < height; h++){
+                
+            }
+        }
         return image;
     }
 
